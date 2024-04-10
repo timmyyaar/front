8e36ee5245f02c46f618cf5abe39d28d3eb46efc
r.boikov <r.boikov@wsc-sports.com>
2024-04-10 08:26:39 +0300

    Server-side sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${process.env.SITE_URL}server-sitemap.xml`, // <==== Add here
+    ],
+  },
 };
