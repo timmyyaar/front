be47a588be4d0261cc11a1e6b85b8b68d0dab66e
r.boikov <r.boikov@wsc-sports.com>
2024-04-10 08:48:22 +0300

    Sitemaps and robots updates

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,29 @@
 module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
+<<<<<<< Updated upstream
+=======
+  exclude: ["/server-sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: [`${process.env.SITE_URL}server-sitemap.xml`],
+  },
+  alternateRefs: [
+    {
+      href: `${process.env.SITE_URL}pl`,
+      hreflang: "pl",
+    },
+    {
+      href: `${process.env.SITE_URL}en`,
+      hreflang: "en",
+    },
+    {
+      href: `${process.env.SITE_URL}ru`,
+      hreflang: "ru",
+    },
+    {
+      href: `${process.env.SITE_URL}ua`,
+      hreflang: "ua",
+    },
+  ],
+>>>>>>> Stashed changes
 };
