62e95cd16500ee8ef030f98ed71aa63946d7366f
r.boikov <r.boikov@wsc-sports.com>
2024-05-04 11:31:31 +0300

    Small update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,40 +17,37 @@ export const metadata: Metadata = {
     "professional cleaning, housekeeping, office cleaning, commercial cleaning, eco-friendly cleaning",
 };
 
-export default function RootLayout({
-  children,
-}: {
+interface RootLayoutProps {
   children: React.ReactNode;
-}) {
+}
 
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-      <html lang="en">
-        <head>
-          <meta
-            name="p:domain_verify"
-            content="a212016f6fcbc5b5b2e70698845b2e51"
-          />
-          <meta
-            name="google-site-verification"
-            content="av2s6aQA-JHPfX_BH2FLGLMDMD-ty5fsLmX4evD3R-E"
-          />
-        </head>
-        <body className={`${inter.className} custom-scroll`}>
-          {children}
-        </body>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    <html lang="en">
+      <head>
+        <meta
+          name="p:domain_verify"
+          content="a212016f6fcbc5b5b2e70698845b2e51"
         />
-        <Script strategy="afterInteractive">
-          {`
+        <meta
+          name="google-site-verification"
+          content="av2s6aQA-JHPfX_BH2FLGLMDMD-ty5fsLmX4evD3R-E"
+        />
+      </head>
+      <body className={`${inter.className} custom-scroll`}>{children}</body>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="afterInteractive">
+        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', { send_page_view: false });
           `}
-        </Script>
-      </html>
+      </Script>
+    </html>
   );
 }
