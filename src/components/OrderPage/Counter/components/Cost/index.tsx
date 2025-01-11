db36e9f6ca0f0c552e2bdb58172158e557f4f64c
Roman <roman.boikov@wsc-sports.com>
2025-01-11 22:28:36 +0100

    Added m2 to ozonation counter

--- src/components/OrderPage/Counter/components/Cost/index.tsx
+++ src/components/OrderPage/Counter/components/Cost/index.tsx
@@ -3,7 +3,8 @@
 import { NUMBER_REGEX } from "@/constants";
 import { getOzonationMultiplier } from "@/utils";
 import React, { useContext } from "react";
-import { PricesContext } from "@/components/Providers";
+import { LocaleContext, PricesContext } from "@/components/Providers";
+import { useLocales } from "@/hooks/useLocales";
 
 interface Props {
   title: string;
@@ -13,6 +14,8 @@ interface Props {
 
 const Cost = ({ title, cost, count }: Props) => {
   const { prices } = useContext(PricesContext);
+  const { locales } = useContext(LocaleContext);
+  const { t } = useLocales(locales);
   const transformedCost = title.toLowerCase().includes("ozonation")
     ? cost.replace(NUMBER_REGEX, String(getOzonationMultiplier(prices, count)))
     : cost;
@@ -24,9 +27,10 @@ const Cost = ({ title, cost, count }: Props) => {
     >
       {transformedCost.indexOf("m2") !== -1 ? (
         <>
-          {transformedCost.replace("m2", "")}
+          {transformedCost.replace(`m2`, "")}
           <>
-            m<sup>2</sup>
+            {t("m")}
+            <sup>2</sup>
           </>
         </>
       ) : (
