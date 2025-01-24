a264b374856ffcdba504af42464c6681fe2fe2fe
Roman <roman.boikov@wsc-sports.com>
2025-01-25 00:08:07 +0100

    Adjustments

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -13,16 +13,20 @@ function Button({
   title,
   isLink,
   isLoading,
+  isSecondary,
   ...props
 }: ButtonProps) {
   return (
     <button
-      className={`_flex _items-center _justify-center _bg-primary hover:_bg-primary-dark
-        active:_bg-primary-dark disabled:_cursor-default disabled:_pointer-events-none
-        disabled:_bg-primary-disabled _text-white _px-6 _py-3.5 lg:_py-4 _transition-all
-        _text-center _font-medium _border-40 _cursor-pointer ${
-          className || ""
-        } ${isLoading ? "loading" : ""}`}
+      className={`_flex _items-center _justify-center disabled:_cursor-default disabled:_pointer-events-none
+        disabled:_bg-primary-disabled _px-6 _py-3.5 lg:_py-4 _transition-all
+        _text-center _font-medium _border-40 _cursor-pointer ${className || ""}
+        ${isLoading ? "loading" : ""}
+        ${
+          isSecondary
+            ? "hover:_bg-primary-dark active:_bg-primary-dark hover:_text-white active:_text-white _z-10 _bg-white _text-dark"
+            : "_text-white _bg-primary hover:_bg-primary-dark active:_bg-primary-dark"
+        }`}
       {...props}
     >
       {title}
